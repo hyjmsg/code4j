45434aca10aeae82e772e9463336d36e5c0f9697
huangyj <huangyj@bsfit.com.cn>
2020-02-27 09:34:34 +0800

    first commit

---
+++ src/main/java/com/learning/code4j/Code4jApplication.java
@@ -0,0 +1,13 @@
+package com.learning.code4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Code4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Code4jApplication.class, args);
+	}
+
+}
